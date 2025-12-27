b38aff6ed03530119a9ee059b8bf73d4517bf156
Sviatobor-ai <hi@boostcore.ai>
2025-12-27 11:02:19 +0100

    refactoring middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,19 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (!request.nextUrl.searchParams.has('q')) {
+  const url = request.nextUrl;
+
+  if (!url.searchParams.has("q")) {
     return NextResponse.next();
   }
 
   const response = NextResponse.next();
-  response.headers.set('X-Robots-Tag', 'noindex, follow');
+  response.headers.set("X-Robots-Tag", "noindex, follow");
   return response;
 }
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
+  ],
+};
